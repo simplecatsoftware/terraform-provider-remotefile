87246e83ebb1a8f2bad1594abe3bc341f8aa128f
Lawrence Goldstien <lawrence@simplecat.software>
2020-01-13 09:38:27 +0000

    fix download after redirects

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,10 +12,10 @@ hash jq 2>/dev/null || { echo >&2 "This install script requires jq to work. ($JQ
 echo "Finding latest release binaries from $GITHUB_REPO for $OS"
 ASSETS_URL=$(curl -s "https://api.github.com/repos/$GITHUB_REPO/releases/latest" | grep "assets_url" | cut -d : -f 2,3 | tr -d \" | tr -d \,)
 
-ASSET_URL=$(curl -L -s $ASSETS_URL | jq -r .[].browser_download_url | grep $OS)
+ASSET_URL=$(curl -s $ASSETS_URL | jq -r .[].browser_download_url | grep $OS)
 
 echo "Found a binary at $ASSET_URL"
 mkdir -p "$DESTINATION_DIR"
-curl -o "$DESTINATION_DIR/$DESTINATION_FILE" $ASSET_URL
+curl -L -o "$DESTINATION_DIR/$DESTINATION_FILE" $ASSET_URL
 
 echo "Finished installing $GITHUB_REPO to $DESTINATION_DIR"
