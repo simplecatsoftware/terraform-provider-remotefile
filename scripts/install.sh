038a35512088ebf4d498bf1747c823f2a4337f48
Lawrence Goldstien <lawrence@simplecat.software>
2020-01-13 09:34:58 +0000

    check if jq is installed on install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,15 @@
 GITHUB_REPO="simplecatsoftware/terraform-provider-remotefile"
 DESTINATION_DIR="$HOME/.terraform.d/plugins/"
 DESTINATION_FILE="terraform-provider-remotefile"
-
 OS=$(uname | tr '[:upper:]' '[:lower:]')
 
-echo "Finding latest release binaries from $REPO for $OS"
+
+[[ $OS == "darwin" ]] && JQ_INSTALL_HELP="brew install jq"
+[[ $OS == "linux" ]] && JQ_INSTALL_HELP="brew install jq"
+
+hash jq 2>/dev/null || { echo >&2 "This install script requires jq to work. ($JQ_INSTALL_HELP) Aborting."; exit 1; }
+
+echo "Finding latest release binaries from $GITHUB_REPO for $OS"
 ASSETS_URL=$(curl -s "https://api.github.com/repos/$GITHUB_REPO/releases/latest" | grep "assets_url" | cut -d : -f 2,3 | tr -d \" | tr -d \,)
 
 ASSET_URL=$(curl -L -s $ASSETS_URL | jq -r .[].browser_download_url | grep $OS)
